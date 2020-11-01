3bbf0d616f78066cbbcee1f3378d855f262bb112
rifqisambas <rifqi@sera5.id>
2020-11-01 11:40:13 +0700

    add intervention/image package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-	dirname(__DIR__)
+  dirname(__DIR__)
 ))->bootstrap();
 
 date_default_timezone_set(env('APP_TIMEZONE', 'Asia/Jakarta'));
@@ -20,7 +20,7 @@ date_default_timezone_set(env('APP_TIMEZONE', 'Asia/Jakarta'));
 */
 
 $app = new Laravel\Lumen\Application(
-	dirname(__DIR__)
+  dirname(__DIR__)
 );
 
 $app->withFacades();
@@ -39,13 +39,13 @@ $app->withEloquent();
 */
 
 $app->singleton(
-	Illuminate\Contracts\Debug\ExceptionHandler::class,
-	App\Exceptions\Handler::class
+  Illuminate\Contracts\Debug\ExceptionHandler::class,
+  App\Exceptions\Handler::class
 );
 
 $app->singleton(
-	Illuminate\Contracts\Console\Kernel::class,
-	App\Console\Kernel::class
+  Illuminate\Contracts\Console\Kernel::class,
+  App\Console\Kernel::class
 );
 
 /*
@@ -95,7 +95,7 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
-
+$app->register(Intervention\Image\ImageServiceProviderLumen::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -109,9 +109,9 @@ $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 */
 
 $app->router->group([
-	'namespace' => 'App\Http\Controllers',
+  'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-	require __DIR__ . '/../routes/web.php';
+  require __DIR__ . '/../routes/web.php';
 });
 
 return $app;
